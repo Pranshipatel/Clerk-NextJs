4a3fcdb10f36da7dedb79fef1385a6b923f34453
Pranshi Patel <patelpranshi745@gmail.com>
2025-02-21 18:01:27 +0530

    sec

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,21 +5,25 @@ import {
     SignedOut,
     UserButton
   } from '@clerk/nextjs'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between '>
-        <ul className='bg-red-500 flex items-center w-full'>
-            <li>Home</li>
-            <li>Contact</li>
-            <li>About</li>
-        </ul>
+    <div className='w-full flex items-center justify-between  p-4'>
+        <h1 className='font-semibold text-xl'>Clerk</h1>
+        <div className=' flex items-center   gap-4'>
+        <Link href="">Home</Link>
+        <Link href="">About</Link>
+            <Link href="">Contact</Link>
+        </div>
+      <div className='flex '>
       <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
+      </div>
     </div>
   )
 }
